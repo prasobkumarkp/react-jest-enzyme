080bf35d0b418070d0fde64657b3c0f32698a0d1
Prasob kumar kp <prasobkumarkp@gmail.com>
2020-08-10 19:55:01 +0530

    Refactored test code

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App" data-test="component-app">
-        <h1>Learn react!!</h1>
+        <h1 data-test="counter-display">The counter is currently</h1>
+        <button data-test="increment-button">Increment counter</button>
       </div>
     );
   }
